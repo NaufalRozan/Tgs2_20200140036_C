29a708da2374978cb5b379324da93125e1054469
Naufal Rozan <naufal.rozan.ft20@mail.umy.ac.id>
2022-03-26 00:51:50 +0700

    Add project files.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Exercise1
+{
+    class Program
+    {
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+        }
+    }
+}
