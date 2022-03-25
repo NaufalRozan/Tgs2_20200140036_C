a24d0e9b52771d22aa8c542de24eabe892b72d45
Naufal Rozan <naufal.rozan.ft20@mail.umy.ac.id>
2022-03-26 01:40:03 +0700

    Create Table Barang dan Transaksi

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -23,7 +23,11 @@ namespace Exercise1
                 SqlCommand pbl = new SqlCommand("create table Pembeli (Id_Pembeli char(5) Primary Key," + "Nama_Pembeli varchar(25), Telepon_Pembeli varchar(13))", con);
                 pbl.ExecuteNonQuery();
 
-                
+                SqlCommand brg = new SqlCommand("create table Barang (Kode_Barang char(5) Primary Key," + "Nama_Barang varchar(20), Harga_Barang int, Jumlah_Barang int)", con);
+                brg.ExecuteNonQuery();
+
+                SqlCommand trk = new SqlCommand("create table Transaksi (Kode_Transaksi char(5) Primary Key," + "Id_Kasir char(5) foreign key references Kasir(Id_Kasir), Id_Pembeli char(5) foreign key references Pembeli(Id_Pembeli), Kode_Barang char(5) foreign key references Barang(Kode_Barang), Tanggal date, Waktu time)", con);
+                trk.ExecuteNonQuery();
             }
         }
     }
